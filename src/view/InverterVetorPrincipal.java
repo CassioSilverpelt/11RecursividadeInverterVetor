3e41a66d0e321d0ca91ce596fadb18897da2fc01
CassioSilverpelt <cassio.vianna@fatec.sp.gov.br>
2024-08-12 22:11:30 -0300

    Update InverterVetorPrincipal.java
    corrigido

--- src/view/InverterVetorPrincipal.java
+++ src/view/InverterVetorPrincipal.java
@@ -8,18 +8,14 @@ public class InverterVetorPrincipal {
 		int[] vetorx = new int[10];
 		int[] vetory = new int[11];		
 		
-		invCon.popularVetor(vetorx, vetorx.length-1);
-		invCon.inverterVetor(vetorx, 0);
+		invCon.popularVetor(vetorx, 0);
+		invCon.inverterVetor(vetorx, 0, vetorx.length-1);
 		invCon.escreverVetor(vetorx);
 		//1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 		
-		invCon.popularVetor(vetory, vetory.length-1);
-		invCon.inverterVetor(vetory, 0);
+		invCon.popularVetor(vetory, 0);
+		invCon.inverterVetor(vetory, 0, vetory.length-1);
 		invCon.escreverVetor(vetory);
 		//1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
-		
-
-
-		
 	}
 }
